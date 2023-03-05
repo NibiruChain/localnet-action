64ced082cf815071a9cfa8e575ad126008c31d1d
Matthias <md2022@matrixsystems.co>
2023-03-06 00:23:37 +0100

    fix: create templates in shell

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -3,6 +3,8 @@
 # set -e
 
 curl -s https://raw.githubusercontent.com/NibiruChain/nibiru/master/contrib/docker-compose/docker-compose-chaosnet.yml >action/docker-compose-chaosnet.yml
+
+mkdir templates
 curl -s https://raw.githubusercontent.com/NibiruChain/nibiru/master/contrib/templates/bdjuno.yaml >templates/bdjuno.yaml
 
 docker compose -f action/docker-compose-chaosnet.yml up $services
