5b64d5ce4c0c105abe86f366f2f8d93078899d24
Matthias <md2022@matrixsystems.co>
2023-03-06 01:33:23 +0100

    wip

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -2,11 +2,12 @@
 
 set -e
 
+echo "service $service"
+echo "ghtoken $ghtoken"
+echo "ghuser $ghuser"
+
 mkdir templates
 mkdir -p action
-
-echo "hola"
-echo $ghtoken
 
 curl -s https://raw.githubusercontent.com/NibiruChain/nibiru/master/contrib/docker-compose/docker-compose-chaosnet.yml >action/docker-compose-chaosnet.yml
 curl -s https://raw.githubusercontent.com/NibiruChain/nibiru/master/contrib/templates/bdjuno.yaml >templates/bdjuno.yaml
