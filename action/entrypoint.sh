7c02fc36d1904cf47c9adb731a5d05295cdfa308
Matthias <md2022@matrixsystems.co>
2023-03-06 01:49:02 +0100

    wip

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -2,23 +2,12 @@
 
 set -e
 
-echo "$1"
-echo "$2"
-echo "$3"
-
-echo "service $input_services"
-echo "ghtoken $input_ghtoken"
-echo "ghuser $input_ghactor"
-echo "SERVICE $INPUT_SERVICES"
-echo "GHTOKEN $INPUT_GHTOKEN"
-echo "GHUSER $INPUT_GHACTOR"
-
 mkdir templates
 mkdir -p action
 
 curl -s https://raw.githubusercontent.com/NibiruChain/nibiru/master/contrib/docker-compose/docker-compose-chaosnet.yml >action/docker-compose-chaosnet.yml
 curl -s https://raw.githubusercontent.com/NibiruChain/nibiru/master/contrib/templates/bdjuno.yaml >templates/bdjuno.yaml
 
-echo $ghtoken | docker login ghcr.io -u $ghactor --password-stdin
+echo $INPUT_GHTOKEN | docker login ghcr.io -u $INPUT_GHACTOR --password-stdin
 
-docker compose -f action/docker-compose-chaosnet.yml up $services
+docker compose -f action/docker-compose-chaosnet.yml up $INPUT_SERVICES
