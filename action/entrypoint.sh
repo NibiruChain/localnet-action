9fe964948512f0037444ac23ac84555e80a15ec7
Matthias <md2022@matrixsystems.co>
2023-03-06 23:19:35 +0100

    fix: try build

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -10,4 +10,4 @@ curl -s https://raw.githubusercontent.com/NibiruChain/nibiru/master/contrib/temp
 
 echo $INPUT_GHTOKEN | docker login ghcr.io -u $INPUT_GHACTOR --password-stdin
 
-docker compose -f action/docker-compose-chaosnet.yml up $INPUT_SERVICES
+docker compose -f action/docker-compose-chaosnet.yml up $INPUT_SERVICES --build
