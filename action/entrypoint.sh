8ccce6f0b957154c054e032b4df6df20607c8218
Matthias <md2022@matrixsystems.co>
2023-03-07 00:04:04 +0100

    wip

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -8,6 +8,9 @@ mkdir -p /github/contrib/docker
 
 curl -s https://raw.githubusercontent.com/NibiruChain/nibiru/master/contrib/docker-compose/docker-compose-chaosnet.yml >action/docker-compose-chaosnet.yml
 curl -s https://raw.githubusercontent.com/NibiruChain/nibiru/master/contrib/docker/chaosnet.Dockerfile >/github/contrib/docker/chaosnet.Dockerfile
+
+curl -s https://raw.githubusercontent.com/NibiruChain/nibiru/master/contrib/scripts/chaosnet.sh >/contrib/scripts/chaosnet.sh
+
 curl -s https://raw.githubusercontent.com/NibiruChain/nibiru/master/contrib/templates/bdjuno.yaml >templates/bdjuno.yaml
 
 echo $INPUT_GHTOKEN | docker login ghcr.io -u $INPUT_GHACTOR --password-stdin
