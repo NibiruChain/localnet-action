dda270826ac3805d5f9056b0edfa09b7fad8030f
Matthias <md2022@matrixsystems.co>
2023-03-06 01:26:54 +0100

    wip: pass through gh token

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/sh -l
 
-# set -e
+set -e
 
 mkdir templates
 mkdir -p action
 
 echo "hola"
-echo $GITHUB_TOKEN
+echo $ghtoken
 
 curl -s https://raw.githubusercontent.com/NibiruChain/nibiru/master/contrib/docker-compose/docker-compose-chaosnet.yml >action/docker-compose-chaosnet.yml
 curl -s https://raw.githubusercontent.com/NibiruChain/nibiru/master/contrib/templates/bdjuno.yaml >templates/bdjuno.yaml
+
+docker login ghcr.io -u $ghactor -p $ghtoken
 
 docker compose -f action/docker-compose-chaosnet.yml up $services
