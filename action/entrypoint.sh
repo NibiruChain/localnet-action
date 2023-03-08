f82217e3985e4bcb618064364505f0a1696aeef8
Matthias <md2022@matrixsystems.co>
2023-03-08 16:45:26 +0100

    fix: use detach

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -8,10 +8,11 @@ mkdir -p /github/contrib/docker
 mkdir -p /contrib/scripts/
 
 curl -s https://raw.githubusercontent.com/NibiruChain/nibiru/master/contrib/docker-compose/docker-compose-chaosnet.yml >action/docker-compose-chaosnet.yml
+curl -s https://raw.githubusercontent.com/NibiruChain/nibiru/master/contrib/scripts/chaosnet.sh >/contrib/scripts/chaosnet.sh
 curl -s https://raw.githubusercontent.com/NibiruChain/nibiru/master/contrib/templates/bdjuno.yaml >templates/bdjuno.yaml
 
 echo $(ls /contrib/scripts)
 
-echo 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 | docker login ghcr.io -u matthiasmat --password-stdin
+echo $INPUT_GHTOKEN | docker login ghcr.io -u $INPUT_GHACTOR --password-stdin
 
 docker compose -f action/docker-compose-chaosnet.yml up $INPUT_SERVICES --detach
