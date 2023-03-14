d17139b317cfdbb3b2477d8b54537aa24f32a049
Matthias <md2022@matrixsystems.co>
2023-03-14 23:48:00 +0100

    fix: wip

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -16,4 +16,5 @@ echo $(ls /contrib/scripts)
 echo $INPUT_GHTOKEN | docker login ghcr.io -u $INPUT_GHACTOR --password-stdin
 
 docker compose -f action/docker-compose-chaosnet.yml up $INPUT_SERVICES --detach
+sleep 30
 docker network ls
