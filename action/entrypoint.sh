c167dc6bc86936cb3a10cd69f003d32777532fe0
Jonathan Gimeno <jgimeno@gmail.com>
2023-03-15 12:33:14 +0100

    add logs to price feeder

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -17,4 +17,4 @@ echo $INPUT_GHTOKEN | docker login ghcr.io -u $INPUT_GHACTOR --password-stdin
 
 docker compose -f action/docker-compose-chaosnet.yml up $INPUT_SERVICES --detach
 sleep 30
-docker network ls
+docker-compose logs pricefeeder -f
